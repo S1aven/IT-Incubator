102faf59acf8d0a1815951e75840015a44153a40
Sergei <rudnev09@mail.ru>
2022-03-27 17:52:38 +0300

    15 - useReducer

---
+++ src/Components/UncontrolledAccordion/Reducer.test.ts
@@ -0,0 +1,38 @@
+import {reducer, StateType, TOGGLE_COLLAPSED} from "./Reducer";
+
+
+test('collapsed should be true', () => {
+
+  const state: StateType = {
+    collapsed: false
+  }
+
+  const newState = reducer(state, {type: TOGGLE_COLLAPSED})
+
+
+  expect(newState.collapsed).toBe(true)
+})
+
+test('collapsed should be false', () => {
+
+  const state: StateType = {
+    collapsed: true
+  }
+
+  const newState = reducer(state, {type: TOGGLE_COLLAPSED})
+
+
+  expect(newState.collapsed).toBe(false)
+})
+
+test('reducer should throw error because action type is incorrect', () => {
+
+  const state: StateType = {
+    collapsed: true
+  }
+
+  // const newState = reducer(state, {type: 'FAKE_TYPE'})
+
+
+  expect(() => {reducer(state, {type: 'FAKE_TYPE'})}).toThrowError()
+})
--- src/Components/UncontrolledAccordion/UncontrolledAccordion.tsx
+++ src/Components/UncontrolledAccordion/UncontrolledAccordion.tsx
@@ -1,6 +1,7 @@
-import React, {useState} from "react";
+import React, {useReducer} from "react";
 import UncontrolledAccordionTitle from "./UncontrolledAccordionTitle/UncontrolledAccordionTitle";
 import UncontrolledAccordionBody from "./UncontrolledAccordionBody/UncontrolledAccordionBody";
+import {reducer, TOGGLE_COLLAPSED} from "./Reducer";
 
 type AccordionPropsType = {
   title: string
@@ -9,16 +10,17 @@ type AccordionPropsType = {
 function UncontrolledAccordion(props: AccordionPropsType) {
   console.log('UncontrolledAccordion rendering');
 
-  const [collapsed, setCollapsed] = useState<boolean>(false)
+  // const [collapsed, setCollapsed] = useState<boolean>(false)
+  const [state, dispatch] = useReducer(reducer, {collapsed: false})
 
   const activeCollapsed = () => {
-    setCollapsed(!collapsed)
+    dispatch({type: TOGGLE_COLLAPSED})
   }
 
   return (
     <div>
       <UncontrolledAccordionTitle activeCollapsed={activeCollapsed} title={props.title}/>
-      {!collapsed && <UncontrolledAccordionBody/>}
+      {!state.collapsed && <UncontrolledAccordionBody/>}
     </div>
   )
 }
